f800925117fee8cbe9f924a48f997fdabd982f28
zaki358 <t.yamazaki.1129@gmail.com>
2022-09-01 01:01:55 +0900

    CSS

--- header.php
+++ header.php
@@ -7,16 +7,13 @@
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <meta name="description" content="portfolio">
    <title>portfolio</title>
-   <script src="../script/pace.js"></script>
-   <script src="../script/loder2.js"></script>
-   
-   
-   
-   <link rel="stylesheet" href="../css/style.css">
+   <!-- <script src="../script/pace.js"></script>
+   <script src="../script/loder2.js"></script> -->
+   <!-- <link rel="stylesheet" href="../css/style.css"> -->
    <link rel="preconnect" href="https://fonts.googleapis.com">
    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-   <link href="https://fonts.googleapis.com/css2?family=Shippori+Mincho:wght@400;500;600;700&display=swap"
+   <!-- <link href="https://fonts.googleapis.com/css2?family=Shippori+Mincho:wght@400;500;600;700&display=swap"
       rel="stylesheet">
    <link href="https://fonts.googleapis.com/css2?family=Potta+One&display=swap" rel="stylesheet">
-   <link rel="stylesheet" href="../scss/object/utility/_loder.scss">
+   <link rel="stylesheet" href="../scss/object/utility/_loder.scss"> -->
 </head>
